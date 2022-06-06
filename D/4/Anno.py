472f0bf95c33fbf8a74c6c92c3469e60a8793aa2
Alex <alejandro.rod.lop@gmail.com>
2022-06-06 18:10:22 +0200

    Added Anno.py instructions

---
+++ D/4/Anno.py
@@ -0,0 +1,17 @@
+"""
+Programa 3:
+    Pide anno actual y otro cualquiera.
+    Mostrar cuantos annos han pasado desde el otro o cuantos faltan.
+
+¿En que anno estamos?: 2015 # NOTA: El acutal es menor que el otro
+Escribe un anno cualquiera: 2020
+Para llegar al 2020 faltan 5 annos
+
+¿En que anno estamos?: 2015 # NOTA: El acutal es mayor que el otro
+Escribe un anno cualquiera: 1997
+Desde el anno 1997 han pasado 18 annos
+
+¿En que anno estamos?: 2015 # NOTA: El acutal es igual que el otro
+Escribe un anno cualquiera: 2015
+¡Son el mismo anno!
+"""

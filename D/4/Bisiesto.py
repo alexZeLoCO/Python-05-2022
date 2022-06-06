430dd8d6485095c7494bbb34d2ef69d52ef25e2d
Alex <alejandro.rod.lop@gmail.com>
2022-06-06 18:27:03 +0200

    Added ISBN and bisiesto

---
+++ D/4/Bisiesto.py
@@ -0,0 +1,15 @@
+"""
+Programa bisiesto:
+    Pide un anno.
+    Un anno es bisiesto cuando este es divisible entre 400 o entre 4 pero no entre 100.
+
+Ejemplo:
+    Introduzca un anno: 800
+    800 es un anno bisiesto.
+
+    Introduzca un anno: 16
+    16 es un anno bisiesto
+
+    Introduzca un anno: 200
+    200 no es un anno bisiesto
+"""
---
+++ D/4/ISBN.py
@@ -0,0 +1,13 @@
+"""
+Programa ISBN:
+    Crea un ISBN pidiendo al usuario los digitos uno a uno.
+    Un ISBN tiene 10 digitos.
+
+Ejemplo:
+    Introduzca digito 1: 1
+    Introduzca digito 2: 2
+    Introduzca digito 3: 3
+    (...)
+    Introduzca digito 10: 0
+    ISBN: 0987654321
+"""

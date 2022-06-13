bbc9b5110bdc4e691c1833b9bcd2c01004ae399b
Alex <alejandro.rod.lop@gmail.com>
2022-06-13 16:36:21 +0200

    Added mayor

--- C/14/module1.py
+++ C/14/module1.py
@@ -13,16 +13,22 @@ menor = 0 # Menor numero introducido
 cantidad = int(input("Introduzca la cantidad de numeros a introducir: "))
 
 for i in range (0, cantidad, 1):
+
     valor = int(input("Introduzca valor: "))
-    media = media + valor
 
     if (valor < menor or i == 0):
         # Si el valor que me dan es mas pequenno que el mas pequenno visto hasta ahora
         # o estamos en la primera vuelta (el primero siempre es el mas pequenno)
         menor = valor # entonces el nuevo mas pequenno es el valor
 
+    if (valor > mayor or i == 0):
+        # Si el valor que me dan es mas grande que el mas grande visto hasta ahora
+        # o estamos en la primera vuelta (el primero siempre es el mas grande)
+        mayor = valor # entonces el nuevo mas grande es el valor
+
+    media = media + valor
+
 media = media / cantidad
 print("El valor mas pequenno introducido es", menor)
+print("El valor mas grande introducido es", mayor)
 print("La media de los numeros introducidos es", media)
-
-
